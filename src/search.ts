b0102106e6660f72191fe2e7b0156ce5c98e6084
Niklas Schönberg <niklas@uplink.fi>
2025-10-31 09:13:12 +0200

    Refactor alias creation and site search logic
    - Simplify createAliases to handle search and default cases separately -
    Extract createSiteAlias helper for site alias objects - Refactor
    getSiteList to use getServerMap helper - Add getServerMap function for
    cleaner server mapping

--- src/search.ts
+++ src/search.ts
@@ -3,30 +3,36 @@ import { Server } from "./types/server";
 import { type CliSite, cliSiteSchema, type Site } from "./types/site";
 import { confirm } from "@topcli/prompts";
 
-async function getSiteList(): Promise<CliSite[]> {
-  const serverMap = {};
+export async function getSiteList(): Promise<CliSite[]> {
   const sites: CliSite[] = [];
+  const serverMap = await getServerMap();
+  const cachedSites = await getCachedSites();
+  cachedSites.forEach((site: Site) => {
+    if (serverMap[site.server_id]) {
+      const serverName = serverMap[site.server_id].split(".")[0];
+      const newSite = cliSiteSchema.parse({
+        id: site.id,
+        name: site.domain,
+        serverId: site.server_id,
+        serverName,
+        ssh: `${site.site_user}@${serverMap[site.server_id]}`,
+        path: "files",
+      });
+
+      sites.push(newSite);
+    }
+  });
+
+  return sites;
+}
+
+async function getServerMap(): Promise<object> {
+  const serverMap = {};
   const cachedServers = await getCachedServers();
   cachedServers.forEach((server: Server) => {
     serverMap[server.id] = server.name;
   });
-
-  const cachedSites = await getCachedSites();
-  cachedSites.forEach((site: Site) => {
-    const serverName = serverMap[site.server_id].split(".")[0];
-    const newSite = cliSiteSchema.parse({
-      id: site.id,
-      name: site.domain,
-      serverId: site.server_id,
-      serverName,
-      ssh: `${site.site_user}@${serverMap[site.server_id]}`,
-      path: "files",
-    });
-
-    sites.push(newSite);
-  });
-
-  return sites;
+  return serverMap;
 }
 
 export async function searchSites(query: string): Promise<CliSite[]> {
