fc1be7d2ff9aeb92894cd52034d9b7443cdc4903
Niklas Schönberg <niklas@uplink.fi>
2025-11-21 15:31:56 +0200

    feat: Add plugin install command and support for repo plugin URLs

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const PACKAGE_NAME = "jman";
 export const API_BASE_URL = "https://api.spinupwp.app/v1";
+export const REPO_PATH =
+  "/wp-content/uploads/satispress-WFTHXqQV9wgE/packages/";
 export enum RequestMethod {
   GET = "GET",
   POST = "POST",
