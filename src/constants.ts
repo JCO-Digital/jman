793acc698a393d22f226b1e95025103b82371366
Niklas Schönberg <niklas@uplink.fi>
2026-01-20 14:54:33 +0200

    feat: Add functions for Slack and WP vulnerabilities
    Introduce new functions to send messages to Slack and retrieve WordPress
    vulnerability data.

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,8 @@ export const PACKAGE_NAME = "jman";
 export const API_BASE_URL = "https://api.spinupwp.app/v1";
 export const REPO_PATH =
   "/wp-content/uploads/satispress-WFTHXqQV9wgE/packages/";
+export const WPVULNERABILITY_API_URL =
+  "https://www.wpvulnerability.net/plugin/";
 export enum RequestMethod {
   GET = "GET",
   POST = "POST",
