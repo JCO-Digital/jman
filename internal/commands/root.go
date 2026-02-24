4f203d4cbc5296baab9461d83dbcaa2209b5a7d5
Niklas Schönberg <niklas@uplink.fi>
2026-02-24 14:41:49 +0200

    fix(root): show version only in verbose mode

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -28,7 +28,7 @@ var rootCmd = &cobra.Command{
 			verbosity.Set(verbosity.Normal)
 		}
 
-		verbosity.Printf(verbosity.Normal, "Version: %s\n", config.RunData.Version)
+		verbosity.Printf(verbosity.Verbose, "Version: %s\n", config.RunData.Version)
 		verbosity.Printf(verbosity.Debug, "Verbosity: %s\n", verbosity.Get())
 	},
 }
