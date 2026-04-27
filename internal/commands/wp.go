d74963b4e2a9fd063efd239ad0d42fb3f7986025
Niklas Schönberg <niklas@uplink.fi>
2026-04-27 16:29:44 +0300

    feat(commands): add shell completion for monitor and wp commands

--- internal/commands/wp.go
+++ internal/commands/wp.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
+	"os/exec"
 
 	"github.com/JCO-Digital/jman/internal/search"
 	"github.com/JCO-Digital/jman/internal/verb"
@@ -13,6 +15,64 @@ var wpCmd = &cobra.Command{
 	Use:   "wp <target> [args...]",
 	Short: "Run a wp-cli command on a target site.",
 	Args:  cobra.MinimumNArgs(2),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) == 0 {
+			sites, err := search.SearchSitesFast(toComplete)
+			if err != nil {
+				return nil, cobra.ShellCompDirectiveError
+			}
+			var completions []string
+			for _, site := range sites {
+				completions = append(completions, fmt.Sprintf("%s\t%s", site.Name, site.ServerName))
+				if site.Name != site.ServerName && site.ServerName != "" {
+					completions = append(completions, fmt.Sprintf("%s\t%s", site.ServerName, site.Name))
+				}
+			}
+			return completions, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		// WP-CLI completion using local wp-cli
+		type wpCommand struct {
+			Name        string      `json:"name"`
+			Description string      `json:"description"`
+			Subcommands []wpCommand `json:"subcommands"`
+		}
+
+		cmdDump := exec.Command("wp", "cli", "cmd-dump", "--format=json")
+		output, err := cmdDump.Output()
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		var dump wpCommand
+		if err := json.Unmarshal(output, &dump); err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		current := dump
+		// Traverse the command tree based on args[1:]
+		for i := 1; i < len(args); i++ {
+			found := false
+			for _, sub := range current.Subcommands {
+				if sub.Name == args[i] {
+					current = sub
+					found = true
+					break
+				}
+			}
+			if !found {
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+		}
+
+		var completions []string
+		for _, sub := range current.Subcommands {
+			completions = append(completions, fmt.Sprintf("%s\t%s", sub.Name, sub.Description))
+		}
+
+		return completions, cobra.ShellCompDirectiveNoFileComp
+	},
+
 	RunE: func(cmd *cobra.Command, args []string) error {
 		target := args[0]
 
